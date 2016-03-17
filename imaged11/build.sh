eecdf49a42bc2778db6e8322008c1ff11ab8fabc
Darren Dale <dsdale24@gmail.com>
2016-03-17 10:49:04 -0400

    workaround numpy issue 7427 on osx

--- imaged11/build.sh
+++ imaged11/build.sh
@@ -1,3 +1,8 @@
 $PYTHON -c "import time; print time.strftime('1.6.0.%Y.%m.%d')" > $SRC_DIR/__conda_version__.txt
 
+# work around https://github.com/numpy/numpy/issues/7427:
+if [ `uname` == Darwin ]; then
+    LDFLAGS="$LDFLAGS -undefined dynamic_lookup -bundle"
+fi
+
 $PYTHON setup.py install
